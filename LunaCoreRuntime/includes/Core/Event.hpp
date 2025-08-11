a15b6c8acbea82d2a450826dc7491f7a89459c4e
STBrian <100104486+STBrian@users.noreply.github.com>
2025-08-10 22:43:25 -0600

    Update Event.hpp

--- LunaCoreRuntime/includes/Core/Event.hpp
+++ LunaCoreRuntime/includes/Core/Event.hpp
@@ -1,36 +1,8 @@
 #pragma once
 
 #include <string>
-#include <vector>
 
 #include "lua_common.h"
-
-enum EventDataFieldType {
-    EVENT_DATA_FLOAT,
-    EVENT_DATA_INT,
-    EVENT_DATA_BOOL,
-    EVENT_DATA_STRING,
-    EVENT_DATA_POINTER
-};
-
-struct EventDataField {
-    const char* name;
-    EventDataFieldType type;
-    union {
-        float* f;
-        int* i;
-        bool* b;
-        const char** s;
-        void** p;
-    } ptr;
-};
-
-struct EventData {
-    int field_count;
-    EventDataField fields[8];
-};
-
-void l_Push_EventData(lua_State* L, const std::vector<EventDataField>& fields);
 
 namespace Core {
     void EventRestartClock();
